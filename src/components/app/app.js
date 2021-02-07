199bd068c43b9c9da64d9b74df9065ebb29647d9
retrydp <thetemon@gmail.com>
2021-02-07 13:21:27 +0200

    passing function as data sources

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-
+import SwapiService from "../../services/swapi-service";
 import Header from "../header";
 import PeoplePage from "../people-page/";
 import RandomPlanet from "../random-planet";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 
 import "./app.css";
 
 export default class App extends Component {
+  swapiService = new SwapiService();
   state = {
     showRandomPlanet: true,
   };
@@ -30,6 +33,24 @@ export default class App extends Component {
           Toggle Random Planet
         </button>
         <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPlanets} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllStarships} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import SwapiService from "../../services/swapi-service";
 import ErrorIndicator from "../error-indicator";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import "./people-page.css";
 
 export default class PeoplePage extends Component {
+  swapiService = new SwapiService();
   state = { selectedPerson: null };
   onPersonSelected = (id) => {
     this.setState({
@@ -20,7 +22,7 @@ export default class PeoplePage extends Component {
     return (
       <div className="row mb2">
         <div className="col-md-6">
-          <ItemList onItemSelected={this.onPersonSelected} />
+          <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople} />
         </div>
         <div className="col-md-6">
           <PersonDetails personId={this.state.selectedPerson} />

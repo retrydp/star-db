37bbed0b8b0149ba77ab0a719fc803acefe60229
retrydp <thetemon@gmail.com>
2021-02-07 14:10:53 +0200

    new Row component

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,6 +3,7 @@ import SwapiService from "../../services/swapi-service";
 import ErrorIndicator from "../error-indicator";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
+import Row from "../row";
 import "./people-page.css";
 
 export default class PeoplePage extends Component {
@@ -19,15 +20,8 @@ export default class PeoplePage extends Component {
   }
   render() {
     if (this.state.hasError) return <ErrorIndicator />;
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople} renderItem={({ name, gender, birthYear }) => `${name} (${gender},  ${birthYear})`} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
-    );
+    const itemList = <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople} renderItem={({ name, gender, birthYear }) => `${name} (${gender},  ${birthYear})`} />;
+    const personDetails = <PersonDetails personId={this.state.selectedPerson} />;
+    return <Row left={itemList} rigth={personDetails} />;
   }
 }
---
+++ src/components/row/row.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Row = ({ left, rigth }) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">{left}</div>
+      <div className="col-md-6">{rigth}</div>
+    </div>
+  );
+};
+
+export default Row;

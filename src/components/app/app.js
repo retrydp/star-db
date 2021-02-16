c1d7ab6acd59600f904b47879c80163bb1ac119d
retrydp <thetemon@gmail.com>
2021-02-16 13:11:06 +0200

    react api context

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ErrorBoundry from "../error-boundry";
-
 import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
-
+import { SwapiServiceProvider } from "../swapi-service-context";
 import { PersonDetails, PlanetDetails, StarshipDetails, PersonList, PlanetList, StarshipList } from "../sw-components";
 
 import "./app.css";
@@ -48,15 +46,23 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
-          <PersonDetails itemId={11} />
-          <PlanetDetails itemId={5} />
-          <StarshipDetails itemId={9} />
-          <PersonList />
-          <StarshipList />
-          <PlanetList />
-        </div>
+        <SwapiServiceProvider value={this.swapiService}>
+          <div className="stardb-app">
+            <Header />
+
+            <PersonDetails itemId={11} />
+
+            <PlanetDetails itemId={5} />
+
+            <StarshipDetails itemId={9} />
+
+            <PersonList />
+
+            <StarshipList />
+
+            <PlanetList />
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   }

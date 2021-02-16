6e349e8ec2373f378870fbd2506e41057ae76c2e
retrydp <thetemon@gmail.com>
2021-02-16 10:03:18 +0200

    ref

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import ErrorIndicator from '../error-indicator/error-indicator';
+import ErrorIndicator from "../error-indicator";
 
 export default class ErrorBoundry extends Component {
-
   state = {
-    hasError: false
+    hasError: false,
   };
 
   componentDidCatch() {
     this.setState({
-      hasError: true
+      hasError: true,
     });
   }
 
   render() {
-
     if (this.state.hasError) {
-      return <ErrorIndicator />
+      return <ErrorIndicator />;
     }
 
     return this.props.children;

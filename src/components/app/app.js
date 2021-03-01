6342b80f2fd14d25aeb8842468de2cc28df7ff3f
retrydp <thetemon@gmail.com>
2021-03-01 11:12:58 +0200

    people routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -39,7 +39,7 @@ export default class App extends Component {
 
               <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
               <Route path="/people" render={() => <h2>People</h2>} exact />
-              <Route path="/people/" component={PeoplePage} />
+              <Route path="/people/:id?" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" exact component={StarshipsPage} />
               <Route
--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -1,19 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { PersonDetails, PersonList } from "../sw-components";
+import { withRouter } from "react-router-dom";
 import Row from "../row";
 
-export default class PeoplePage extends Component {
-  state = {
-    selectedItem: null,
-  };
+const PeoplePage = ({ history, match }) => {
+  const { id } = match.params;
 
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  };
+  return <Row left={<PersonList onItemSelected={(id) => history.push(id)} />} right={<PersonDetails itemId={id} />} />;
+};
 
-  render() {
-    const { selectedItem } = this.state;
-
-    return <Row left={<PersonList onItemSelected={this.onItemSelected} />} right={<PersonDetails itemId={selectedItem} />} />;
-  }
-}
+export default withRouter(PeoplePage);
